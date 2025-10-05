20391175397a23c1af32f83826b8c7839fd13382
David Kusi <kusidavidkwesi@gmail.com>
2025-10-05 20:25:00 +0000

    Changed the network host

--- app/db.py
+++ app/db.py
@@ -1,31 +1,4 @@
 # # app/db.py
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import Session, declarative_base, sessionmaker
-
-# from app.settings import settings
-
-# # Get DB URL from .env through settings.py
-# DATABASE_URL = settings.DATABASE_URL
-
-# # Create SQLAlchemy engine
-# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
-# # Session
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# # Base class
-# Base = declarative_base()
-
-
-# # Dependency for FastAPI routes
-# def get_db():
-#     db: Session = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
 import os
 
 from sqlalchemy import create_engine
